a0e9891d6ede6f749f1ba9bc00ebc28b25989e0c
Miguel Ángel García Bello <magarbello@gmail.com>
2026-06-23 17:57:18 +0200

    Modify temperature route to return users
    Updated the temperature route to return user data instead of meteorological data. Removed the POST method for temperature submissions.

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, url_for, Blueprint, current_app
 from flask_cors import CORS
 from sqlalchemy import select, func
-from api.models import db, Meteorological, Recommendation
+from api.models import db, User
 
 api = Blueprint('api', __name__)
 
@@ -10,38 +10,8 @@ CORS(api)
 
 @api.route('/temperature', methods=['GET'])
 def get_temperature():
-    result = Meteorological.query.filter_by(freak='temperatura').all()
-    return jsonify([f.serialize() for f in result]), 200
+    result = User.query.all()
+    return jsonify([u.serialize() for u in result]), 200
 
 
-@api.route('/temperature', methods=['POST'])
-def post_temperature():
-    data = request.get_json()
-    
 
-    if not data:
-        return jsonify({'error': 'All fields are required'}), 401
-
-    recommendations = data.get("recomendation_list")
-    new_meteorological = Meteorological(
-        freak=data.get("freak", "temperatura"),
-        cat=data.get("cat"),
-        title=data.get("title"),
-        recommendation=data.get("recommendation"),
-    )
-    
-    db.session.add(new_meteorological)
-    db.session.commit()
-    
-    for recommendation in recommendations:
-        new_recommendation = Recommendation(
-            freak_id= new_meteorological.id,
-            recommendation= recommendation
-        )
-        db.session.add(new_recommendation)
-        db.session.commit()
-
-    return jsonify({
-        "msg": "Created successfully",
-        "recommendation": new_meteorological.serialize()
-    }), 201
